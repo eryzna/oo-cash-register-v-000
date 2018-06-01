f1cef35b14b2597bd8db365c444252c1f69ed169
Elana Ryznar <eryzna@gmail.com>
2018-06-01 15:10:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+  attr_accessor :total
+  def initialize (discount=0)
+    @total=0
+    @discount=discount
+    def discount
+      @discount
+    end
+  end
 
+
+
+  def add_item (title,price,quantity=0)
+    if quantity > 0
+      self.total += price*quantity
+    else
+      self.total += price
+    end
+  end
+
+  def apply_discount
+    self.total*30
+  end
+
+
+end
